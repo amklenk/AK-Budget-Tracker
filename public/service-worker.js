d50a22694d7946cead98d29f135b056886358d1c
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-17 14:54:00 -0400

    set up service worker, caches files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,8 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
-  "./assets/css/style.css",
-  "./assets/js/index.js"
+  "./css/styles.css",
+  "./js/index.js"
 ];
 
 // Respond with cached resources
